346958c3d9a7c00ffe3154fa507a5e272876d01d
m10x.de <maximilian.hildebrand@gmx.de>
2018-03-01 21:13:06 +0100

    Update 5kr1p7.ps1

--- 5kr1p7.ps1
+++ 5kr1p7.ps1
@@ -1,33 +1,58 @@
-param([string]$Kommentar, [int]$Sleep)
+param([string]$Kommentar, [int]$Sleep=1337, [int]$RestoreClipboard=1337)
 
-# Default Sleep Time #
-$sleeptime="3"
-
-# Default Name #
-$name = "Maximilian"
+#############################
+# Default Restore Clipboard #
+$restore = $true            #
+#                           #
+#    Default Sleep Time     #
+$defsleep="3"               #
+#                           #
+#       Default Name        #
+$name = "Maximilian"        #
+#############################
 
 $clip = Get-Clipboard
 $date = Get-Date -UFormat "%H:%M, %d.%m.%Y"
-$kommi=""
+$kommi = ""
+$sleeptime = $defsleep
 if($Kommentar -ne "")
 {
-	$kommi=", $Kommentar"
+	$kommi = ", $Kommentar"
 }
-if($Sleep -ne "")
+if($Sleep -ne 1337 -and $Sleep -ne 1)
 {
-	$sleeptime=$Sleep
+	$sleeptime = $Sleep
 }
 elseif($Kommentar -match "^\d+$")
 {
-	$sleeptime=$Kommentar
-	$kommi=""
+	$sleeptime = $Kommentar
+	$kommi = ""
 }
+if ($Kommentar -eq 0 -or $Kommentar -eq 1)
+{
+	$restore = $Kommentar
+	$kommi = ""
+	$sleeptime = $defsleep
+}
+elseif ($Sleep -eq 0 -or $Sleep -eq 1)
+{
+	$restore = $Sleep
+}
+elseif($RestoreClipboard -ne 1337)
+{
+	$restore = $RestoreClipboard
+}
+
 $output = "$name, $date$kommi"
 Write-Output $output | Set-Clipboard
 Write-Host 
 Get-Clipboard
-Write-Host 
-Write-Host "+++ sleep $sleeptime seconds +++"
-Start-Sleep -s $sleeptime
-Write-Output $clip | Set-Clipboard
-Write-Host "+++ sleepover +++"
+Write-Host
+
+if ($restore -eq $true -or $restore -eq 1)
+{
+	Write-Host "++++ sleep $sleeptime seconds ++++"
+	Start-Sleep -s $sleeptime
+	Write-Output $clip | Set-Clipboard
+	Write-Host "+++ restored Clipboard +++"
+}
